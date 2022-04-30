90318dae4648dbaaed7c6c612a4ba7d472793e35
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-04-30 20:43:54 -0300

    criando tema global da aplicação

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+import { Container, Title } from "./styles";
+
+export function Home() {
+  return (
+    <Container>
+      <Title>sadasd</Title>
+    </Container>
+  );
+}
