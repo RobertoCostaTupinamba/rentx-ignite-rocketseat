776c46e179e0e22e9f526f2fbe521922855ffd01
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-05-01 21:31:11 -0300

    Header criado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { View } from "react-native";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Title } from "./styles";
+import Logo from "../../assets/logo.svg";
+
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 export function Home() {
   return (
     <Container>
-      <Title>sadasd</Title>
+      <StatusBar
+        barStyle="light-content" // dark-content, light-content and default
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
@@ -6,4 +7,25 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text``;
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.headers};
+
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+
+  font-family: ${({ theme }) => theme.fonts.secondary_400};
+  color: ${({ theme }) => theme.colors.text};
+`;
