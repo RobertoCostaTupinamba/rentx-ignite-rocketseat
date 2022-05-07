93e69930b74984dd0e5eaa822a67bd64f363fe47
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-05-07 16:56:14 -0300

    Interface de Agendamento Concluído

--- App.tsx
+++ App.tsx
@@ -18,6 +18,7 @@ import AppLoading from "expo-app-loading";
 import theme from "./src/styles/theme";
 import { Scheduling } from "./src/screens/Scheduling";
 import { SchedulingDetails } from "./src/screens/SchedulingDetails";
+import { SchedulingComplete } from "./src/screens/SchedulingComplete";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -34,7 +35,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
