034e3fc29d2d9d0b49eb98ce9e24c753c9f83223
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-06-02 21:06:40 -0300

    Listando Agendamentos do Usuário

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
+import { CarDTO } from "../../dtos/CarDTO";
+import { api } from "../../services/api";
 
 export function MyCars() {
-  return <View />;
+  const [cars, setCars] = React.useState<CarDTO[]>([]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    async function loadCars() {
+      try {
+        const response = await api.get("/schedules_byuser?user_id=1");
+
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadCars();
+  }, []);
+
+  return <Container></Container>;
 }
