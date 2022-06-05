2e61d7d30dd65c6b8922873da3c426307a64b408
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-06-05 20:02:28 -0300

    Brincando com animações

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,8 +1,52 @@
 import React from "react";
+import { Button, StyleSheet, Dimensions } from "react-native";
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from "react-native-reanimated";
+
+const WIDTH = Dimensions.get("window").width;
+
 import { View } from "react-native";
 
 import { Container } from "./styles";
 
 export function Splash() {
-  return <Container />;
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bezier(0.4, 0, 0.2, 1),
+          }),
+        },
+      ],
+    };
+  });
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * (WIDTH - 100);
+  }
+
+  return (
+    <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Mover" onPress={handleAnimationPosition} />
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+  },
+});
