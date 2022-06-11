afb3fe7f3faf16acb6fd3c3a9244172237ae8749
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-06-11 20:22:13 -0300

    Input de Senha

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar, View } from "react-native";
 import { useTheme } from "styled-components";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
 
 import { Container, Header, SubTitle, Title, Form, Footer } from "./styles";
 
@@ -31,6 +32,12 @@ export function SignIn() {
           iconName="mail"
           placeholder="E-mail"
           keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+        <PasswordInput
+          iconName="lock"
+          placeholder="Senha"
           autoCorrect={false}
           autoCapitalize="none"
         />
