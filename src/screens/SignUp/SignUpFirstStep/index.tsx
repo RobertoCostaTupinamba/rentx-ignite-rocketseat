aa30a5ffba129a82c20598f5332cf4eb5f4181ef
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-06-12 19:45:08 -0300

    Primeiro Step

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -13,6 +13,10 @@ import {
   FormTitle,
 } from "./styles";
 import { Bullet } from "../../../components/Bullet";
+import { Input } from "../../../components/Input";
+import { Button } from "../../../components/Button";
+import { Keyboard, KeyboardAvoidingView } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 export function SignUpFirstStep() {
   const navigation = useNavigation<screenProp>();
@@ -22,27 +26,36 @@ export function SignUpFirstStep() {
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
 
-      <Title>
-        Crie sua{"\n"}
-        conta
-      </Title>
-      <SubTitle>
-        Faça seu cadastro de {"\n"}
-        forma rápida e fácil
-      </SubTitle>
+          <Title>
+            Crie sua{"\n"}
+            conta
+          </Title>
+          <SubTitle>
+            Faça seu cadastro de {"\n"}
+            forma rápida e fácil
+          </SubTitle>
 
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input iconName="user" placeholder="Nome" />
+            <Input iconName="mail" placeholder="E-mail" />
+            <Input iconName="credit-card" placeholder="CNH" />
+          </Form>
+
+          <Button title="Próximo" />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
